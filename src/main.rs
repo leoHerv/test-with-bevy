6d272d47038ca381cddea6d22e82330c963a849f
Léo Hervouet <leoohervouet@gmail.com>
2023-08-05 12:10:03 +0200

    Add basic player and camera

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,52 @@
 use bevy::prelude::*;
+use bevy::window::PrimaryWindow;
 
 fn main() {
-    println!("Hello, world!");
+    App::new()
+        .add_plugins(DefaultPlugins)
+        .add_startup_system(spawn_player)
+        .add_startup_system(spawn_camera)
+        .run();
+}
+
+// A player
+#[derive(Component)]
+pub struct Player
+{
+
+}
+
+// System : to spawn a player in the game
+pub fn spawn_player(
+    mut commands: Commands, 
+    window_query: Query<&Window, With<PrimaryWindow>>,
+    asset_server: Res<AssetServer>,
+)
+{
+    let window: &Window = window_query.get_single().unwrap();
+
+    commands.spawn((
+            SpriteBundle{
+                transform: Transform::from_xyz(window.width() / 2.0, window.height() / 2.0, 0.0),
+                texture: asset_server.load("sprites/ball_blue_large.png"),
+                ..default()
+            },
+            Player{},
+    ));
+}
+
+// System : to spawn a camera in the game
+pub fn spawn_camera(
+    mut commands: Commands, 
+    window_query: Query<&Window, With<PrimaryWindow>>,
+)
+{
+    let window: &Window = window_query.get_single().unwrap();
+
+    commands.spawn(
+        Camera2dBundle{
+            transform: Transform::from_xyz(window.width() / 2.0, window.height() / 2.0, 0.0),
+            ..default()
+        }
+    );
 }
